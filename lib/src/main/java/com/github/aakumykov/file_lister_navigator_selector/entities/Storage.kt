9722992628ac22b0088e566af90df6188630ee79
Andrey Kumykov <aakumykov@yandex.ru>
2024-07-18 14:47:10 +0300

    Удалил устаревший ListAdapter.

--- lib/src/main/java/com/github/aakumykov/file_lister_navigator_selector/entities/Storage.kt
+++ lib/src/main/java/com/github/aakumykov/file_lister_navigator_selector/entities/Storage.kt
@@ -1,10 +1,8 @@
 package com.github.aakumykov.file_lister_navigator_selector.entities
 
-import com.github.aakumykov.file_lister_navigator_selector.utils.ListAdapter
+import androidx.annotation.DrawableRes
 
 data class Storage(
     val label: String,
     val absolutePath: String,
-) : ListAdapter.TitleItem {
-    override fun getTitle(): String = label
-}
+)
--- lib/src/main/java/com/github/aakumykov/file_lister_navigator_selector/file_selector/StorageListAdapter.kt
+++ lib/src/main/java/com/github/aakumykov/file_lister_navigator_selector/file_selector/StorageListAdapter.kt
@@ -1,18 +1,11 @@
 package com.github.aakumykov.file_lister_navigator_selector.file_selector
 
-import android.content.Context
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import androidx.annotation.LayoutRes
 import com.github.aakumykov.file_lister_navigator_selector.R
 import com.github.aakumykov.file_lister_navigator_selector.entities.Storage
-import com.github.aakumykov.file_lister_navigator_selector.utils.ListAdapter
 import com.github.aakumykov.file_lister_navigator_selector.utils.ListHoldingListAdapter
 
-class StorageListAdapter
-    : ListHoldingListAdapter<Storage, StorageListItemViewHolder>(R.layout.storage_list_item)
+class StorageListAdapter : ListHoldingListAdapter<Storage, StorageListItemViewHolder>(R.layout.storage_list_item)
 {
     override fun createViewHolder(itemView: View): ViewHolder<Storage> {
         return StorageListItemViewHolder()
