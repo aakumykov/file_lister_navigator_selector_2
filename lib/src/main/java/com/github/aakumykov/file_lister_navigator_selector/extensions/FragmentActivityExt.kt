5bfabb147890cb8f994c43ef78cf72fe84f49825
Andrey Kumykov <aakumykov@yandex.ru>
2024-12-26 12:34:29 +0300

    Удалил неиспользуемые расширения из модуля lib.

--- lib/src/main/java/com/github/aakumykov/file_lister_navigator_selector/extensions/FragmentActivityExt.kt
+++
@@ -1,8 +0,0 @@
-package com.github.aakumykov.file_lister_navigator_selector.extensions
-
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentResultListener
-
-fun FragmentActivity.listenForFragmentResult(requestKey: String, listener: FragmentResultListener) {
-    supportFragmentManager.setFragmentResultListener(requestKey, this, listener)
-}
--- lib/src/main/java/com/github/aakumykov/file_lister_navigator_selector/extensions/TextViewExt.kt
+++
@@ -1,9 +0,0 @@
-package com.github.aakumykov.file_lister_navigator_selector.extensions
-
-import android.widget.TextView
-import androidx.annotation.ColorRes
-import androidx.core.content.ContextCompat
-
-fun TextView.setColorResource(@ColorRes colorRes: Int) {
-    setTextColor(ContextCompat.getColor(context, colorRes),)
-}
